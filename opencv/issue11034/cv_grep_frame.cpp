aa7cb8f6082e8fb87991875bc26cf6573a01a416
Sayed Adel <seiko@imavr.com>
2018-03-26 02:50:00 +0000

    opencv/opencv#11034: changes
     * Add start pos
     * Add imgcodecs test

--- opencv/issue11034/cv_grep_frame.cpp
+++ opencv/issue11034/cv_grep_frame.cpp
@@ -17,7 +17,7 @@ static uint64_t mat_sum(const Mat& mat)
     len /= sizeof(int64_t);
 
     for (; len > 0; len--)
-        sum += d64[len];
+       sum += d64[len];
 
     return (uint64_t)sum;
 }
@@ -25,9 +25,10 @@ static uint64_t mat_sum(const Mat& mat)
 int main(int argc, char **argv)
 {
     const String opts =
-        "{video  v |    | video file }"
-        "{output o |    | frames's output}"
-        "{sec    s | 60 | frame per second}"
+        "{video  v |    | video path }"
+        "{output o |    | frames output path}"
+        "{fes    f | 60 | frame every [x] second}"
+        "{pos    p | 0  | start pos in second}"
         ;
 
     CommandLineParser parser(argc, argv, opts);
@@ -35,7 +36,8 @@ int main(int argc, char **argv)
 
     String video = parser.get<String>("video");
     String output = parser.get<String>("output");
-    int psec = parser.get<int>("sec");
+    int fes = parser.get<int>("fes");
+    int pos = parser.get<int>("pos");
 
     if (!parser.check())
     {
@@ -43,25 +45,25 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    CV_Assert(!video.empty() && !output.empty() && psec > 0);
+    CV_Assert(!video.empty() && !output.empty() && fes > 0);
 
     VideoCapture cap(video, CAP_FFMPEG);
     CV_Assert(cap.isOpened());
 
-    int fps = (int)cap.get(CAP_PROP_FPS);
-    CV_Assert(fps > 0);
+    int v_fps = (int)cap.get(CAP_PROP_FPS);
+    CV_Assert(v_fps > 0);
 
     int frame_count = (int)cap.get(CAP_PROP_FRAME_COUNT);
     CV_Assert(frame_count > 0);
 
     std::vector<int> png_params, jpg_params;
     png_params.push_back(IMWRITE_PNG_COMPRESSION);
-    png_params.push_back(9);
+    png_params.push_back(3);
     jpg_params.push_back(IMWRITE_JPEG_CHROMA_QUALITY);
 
     char buf[32];
     Mat frame;
-    for (int fpos = 0, count = 1; fpos < frame_count; fpos += fps * psec, count++)
+    for (int fpos = v_fps * pos + v_fps - 1, count = 1; fpos < frame_count; fpos += v_fps * fes, count++)
     {
         CV_Assert(cap.set(CAP_PROP_POS_FRAMES, fpos));
         CV_Assert(cap.read(frame));
@@ -72,7 +74,7 @@ int main(int argc, char **argv)
         sprintf(buf, "/%d.jpg", count);
         CV_Assert(imwrite(output + buf, frame, jpg_params));
 
-        printf("Extract frame(%d) %d, Mat's sum %016llx \n", count, fpos, mat_sum(frame));
+        printf("Extract frame(%d) %d, Mat's sum %llu \n", count, fpos, mat_sum(frame));
     }
     return 0;
 }
